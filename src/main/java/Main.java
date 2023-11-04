c93a3549c129926542ebd1b92d5ac95a3aa7c7f5
GeiseR98 <ipatovsergejj@yandex.ru>
2023-11-04 06:58:23 +0300

    умняш с минимальными уровнями сложности

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        int score = 0;
+        while (true) {
+            if (game.game(score)) {
+                score++;
+            } else {
+                System.out.println("Ваш счёт : " + score);
+                break;
+            }
+        }
+    }
+}
+
